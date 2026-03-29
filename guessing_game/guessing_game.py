d7785f35c88516c74d0ae6eb62fb7140c0125182
abdi221 <abdifatahali22@gmail.com>
2026-03-29 16:25:25 +0200

    Fixed the issues

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -8,14 +8,11 @@ while True:
     if user == computer and count <= 4:
         print(f"You guessed it correctly! And under 3 tries!! The correct number was {computer}")
         break
-    elif user < computer:
-        count += 1
-        print(f"Wrong Guess. Too low. Tries {count} of 5")
-    elif user > computer:
-        count += 1
-        print(f"Wrong Guess. Too high. Tries {count} of 5")
-    elif count == 5:
-        print("Out of tries and not guessed correctly")
+    count += 1
+    if count == 5:
+        print(f"Out of tries and not guessed correcly. {count} of 5")
         break
+    elif user < computer:
+        print(f"Wrong Guess. Too low. Tries {count} of 5")
     else:
-        print("Not a valid input or logic is flawed")
+        print(f"Wrong Guess. Too high. Tries {count} of 5")
