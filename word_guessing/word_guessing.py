6851a6775ee6100633263be9010609c169c5a568
abdi221 <abdifatahali22@gmail.com>
2026-04-02 01:37:01 +0200

    Nearly done, needs sleep tho....

--- word_guessing/word_guessing.py
+++ word_guessing/word_guessing.py
@@ -1,14 +1,26 @@
-# document: created 10 animals from a list and printed one randomly
-# Then I created a loop which would take a random animal from my list and printing each letter one at a time
 import random
 list = ['elephant', 'tiger', 'axolotl', 'platypus', 'blue whale', 'penguin', 'pangolin', 'narwhal', 'honey badger', 'glass frog']
-l = ['elephant', 'tiger', 'axolotl']
 # print(*random.choice(l)) 
 
 # NOTE: for later remeber to maybe use sample() for unique non duplicate values..
-for letter in l: # now i can see that by using the 'in' keyword, i can search for single letter in the word
-    print(letter)
-    if "a" in letter:
-        print("Yes")
-    else:
-        print("No")
+
+# NOTE: use enumerate for better indexing method!! ↓ this ↓↓↓↓↓
+
+comp_word = random.choice(list) # this picks a random word (whole word) from the list
+print(comp_word)
+new_list = []
+count = 0 # to keep count
+while count < 6:
+    for i in comp_word:
+        user = input("Guess a letter: ")
+        count += 1
+        if user in comp_word:
+            new_list.append(i)
+        else:
+            new_list.append('_')
+        # if count == 6:
+        #     print("out of tries\n")
+        break
+print("Here is the words you tried out:")
+print(*new_list, end="")
+print()
