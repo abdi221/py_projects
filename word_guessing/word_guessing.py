4974ae08572a4285a199561a22c30689f8c8b882
abdi221 <abdifatahali22@gmail.com>
2026-04-04 15:35:26 +0200

    finished with the objective of word guessing game & code clean up

--- word_guessing/word_guessing.py
+++ word_guessing/word_guessing.py
@@ -1,26 +1,18 @@
 import random
 list = ['elephant', 'tiger', 'axolotl', 'platypus', 'blue whale', 'penguin', 'pangolin', 'narwhal', 'honey badger', 'glass frog']
-# print(*random.choice(l)) 
 
 # NOTE: for later remeber to maybe use sample() for unique non duplicate values..
 
 # NOTE: use enumerate for better indexing method!! ↓ this ↓↓↓↓↓
 
-comp_word = random.choice(list) # this picks a random word (whole word) from the list
-print(comp_word)
-new_list = []
-count = 0 # to keep count
-while count < 6:
-    for i in comp_word:
-        user = input("Guess a letter: ")
-        count += 1
-        if user in comp_word:
-            new_list.append(i)
-        else:
-            new_list.append('_')
-        # if count == 6:
-        #     print("out of tries\n")
-        break
-print("Here is the words you tried out:")
-print(*new_list, end="")
-print()
+random_word = random.choice(list).replace(" ", "")
+user = input("Guess a letter: ")
+letter_count = 0
+for i in random_word:
+    if user == i:
+        letter_count += 1
+try:
+    letter_count > 0
+    print(f"The letter {user} appears {letter_count} times in {random_word}")
+except:
+    print(f"The letter {user} appears {letter_count} times in {random_word}")
