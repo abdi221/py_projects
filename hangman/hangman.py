648d7c905c21bc0c1a75a6687197a90016983347
abdi221 <abdifatahali22@gmail.com>
2026-05-03 14:14:15 +0200

    Update

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,11 +1,63 @@
 import random
+def hangman():
+  hangmanpics = [r''' #adding r tells the interpreter to look at the figures as they are seen and not new lines
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+  =========''', r'''
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+  =========''', r'''
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+  =========''', r'''
+    +---+
+    |   |
+    O   |
+  /|   |
+        |
+        |
+  =========''', r'''
+    +---+
+    |   |
+    O   |
+  /|\  |
+        |
+        |
+  =========''', r'''
+    +---+
+    |   |
+    O   |
+  /|\  |
+  /    |
+        |
+  =========''', r'''
+    +---+
+    |   |
+    O   |
+  /|\  |
+  / \  |
+        |
+  =========''']
+
 list = ['Elephant', 'Tiger', 'Axolotl', 'Platypus', 'Blue Whale', 'Penguin', 'Pangolin', 'Narwhal', 'Honey Badger', 'Glass Frog']
 
 random_word = random.choice(list).lower()
 print(random_word)
 
 def someword(word):
-  mylist = []
+  mylist = [] # this stores my letters in order
   for i, v in enumerate(random_word.replace(" ", ""), start=1):
       print("_" * i) # hopefully this would print _ _ _ at the start of the program
       if word == v:
@@ -15,61 +67,13 @@ def someword(word):
         mylist.append(shieet)
   return " ".join(mylist)
 
-
-guess = 0 #ei amount of tries
-all_guessed_words = {} #set because its smarter and it doesnt care about the order
-while all_guessed_words != len(random_word):
-   user = input("Guess a letter")
-   all_guessed_words.add(user)
-   someword(user)
-   break
-hangmanpics = [r''' #adding r tells the interpreter to look at the figures as they are seen and not new lines
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', r'''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', r'''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', r'''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
+def main():
+  hangman()
+  guess = 0 #ei amount of tries
+  all_guessed_words = {*()} #set because its smarter and it doesnt care about the order
+  while guess <= len(random_word) + 2:  
+    letter = input("Guess a letter")
+    all_guessed_words.add(letter)
+    guess += 1
+    if letter not in random_word:
+      print(hangman())
